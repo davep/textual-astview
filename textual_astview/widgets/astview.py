72075e4c3a61d88a092b0feb092579a468889efc
Dave Pearson <davep@davep.org>
2023-02-16 20:15:13 +0000

    :fire: Remove an unused import

--- textual_astview/widgets/astview.py
+++ textual_astview/widgets/astview.py
@@ -19,7 +19,6 @@ from rich.text import Text
 # Textual imports.
 from textual.widgets      import Tree
 from textual.widgets.tree import TreeNode
-from textual.message      import Message
 
 ##############################################################################
 ASTNode = TreeNode[ Any ]
